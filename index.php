2cb13a924d7368232115d92d4dbb9d9953f0bbee
Bernardo Corte Real <bernardocortereal@Bernardo.local>
2016-08-01 15:45:01 -0300

    Instalação Boostrap

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Postits</title>
-	<link rel="stylesheet" href="/postits/view/css/style.css">
+    <title>Postits</title>
+    <link rel="stylesheet" href="bower_components/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/postits/view/css/style.css">
 </head>
 <body>
-	
+    
 </body>
 </html>
