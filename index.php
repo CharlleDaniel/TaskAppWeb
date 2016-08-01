e4cb3a85d097e68005a07511435a31676f1ceead
Bernardo Corte Real <bernardocortereal@Bernardo.local>
2016-08-01 16:12:54 -0300

    Modal Included

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Postits</title>
-    <link rel="stylesheet" href="bower_components/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="/postits/view/css/style.css">
-</head>
-<body>
-    
-</body>
+    <head>
+        <title>Postits</title>
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css">
+        <link rel="stylesheet" href="./view/css/style.css">
+    </head>
+    <body>
+        <a class="btn btn-primary" data-toggle="modal" href='#modal-id'>Trigger modal</a>
+        <div class="modal fade" id="modal-id">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                    Title: <input type="text" placeholder=""> <br> <br>
+                    Text:  <input type="text" placeholder="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+        <script src="bower_components/jquery/dist/jquery.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    </body>
 </html>
